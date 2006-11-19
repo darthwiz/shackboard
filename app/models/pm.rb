2c4cc71724a1d4999cea710b1662f3ebd9141896
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2006-11-19 05:08:18 +0000

    New feature: unread message count in the main page (with link).
    git-svn-id: file:///var/lib/svn/shackboard/shackboard/trunk@43 6c9d4111-2920-0410-ac48-c0788669fd8e

--- app/models/pm.rb
+++ app/models/pm.rb
@@ -1,4 +1,8 @@
 class Pm < ActiveRecord::Base
   set_table_name table_name_prefix + "u2u"
   set_primary_key "u2uid"
+  def Pm.unread_for(user) # {{{
+    return nil unless user.is_a? User
+    Pm.count(:conditions => ['msgto = ? AND status = ?', user.username, 'new'])
+  end # }}}
 end
