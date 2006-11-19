3d3bfd0af18cee086e966751e2806dc4cffc6d26
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2006-11-19 19:32:57 +0000

    - Theme's CSS is now referenced by its URI instead of the data in the user's   session file (so that it makes sense to retrieve the dynamically generated   CSS without session information). - Nice little "valid XHTML" button in the home page. Heh. ;-)
    git-svn-id: file:///var/lib/svn/shackboard/shackboard/trunk@45 6c9d4111-2920-0410-ac48-c0788669fd8e

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
   end # }}}
   def css # {{{
     @headers["Content-Type"] = 'text/css; charset = utf-8'
+    @theme_name              = params[:id].sub(/\.css$/, "")
     render :partial => 'css'
   end # }}}
 end
