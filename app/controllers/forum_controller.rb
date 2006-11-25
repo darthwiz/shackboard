e39c008f0f17e86046f18b347440bba8792a28c0
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2006-11-25 00:47:18 +0000

    Topic list implemented with a big list instead of a table -- should be quicker to render. CSS still needs some work.
    git-svn-id: file:///var/lib/svn/shackboard/shackboard/trunk@55 6c9d4111-2920-0410-ac48-c0788669fd8e

--- app/controllers/forum_controller.rb
+++ app/controllers/forum_controller.rb
@@ -47,4 +47,9 @@ class ForumController < ApplicationController
   def tree # {{{
     @tree = Forum.tree
   end # }}}
+  def css # {{{
+    @headers["Content-Type"] = 'text/css; charset = utf-8'
+    @theme_name              = params[:id].sub(/\.css$/, "")
+    render :partial => 'css'
+  end # }}}
 end
