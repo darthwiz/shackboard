54780309bef295f1aced87d81d82efe68415cddb
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2007-10-04 21:02:58 +0000

    M    routes.rb   - Added a rule to let the dynamic stylesheets work with the new Rails version.
    git-svn-id: file:///var/lib/svn/shackboard/shackboard/trunk@178 6c9d4111-2920-0410-ac48-c0788669fd8e

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/service.wsdl', :action => 'wsdl'
 
   # Install the default route as the lowest priority.
+  map.connect ':controller/css/:id.css', :action => 'css'
   map.connect ':controller/:action/:id'
 end
