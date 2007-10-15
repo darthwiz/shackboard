f661c2f278fdc5e704b964cbb380b11a9473475e
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2007-10-15 22:25:54 +0000

    M    app/models/online_user.rb   - Removed Vim marks.   - Used a join to get all the relevant user data (i.e. the uid) in a single     query.
    git-svn-id: file:///var/lib/svn/shackboard/shackboard/trunk@184 6c9d4111-2920-0410-ac48-c0788669fd8e

--- app/models/online_user.rb
+++ app/models/online_user.rb
@@ -4,41 +4,45 @@ class OnlineUser < ActiveRecord::Base
   set_table_name table_name_prefix + "whosonline"
   @@guest  = 'xguest123'
   @@record = 'onlinerecord'
+  #belongs_to :user, :foreign_key => 'username'
   #set_primary_key "username"
   #validates_uniqueness_of :username
-  def OnlineUser.touch(user, ip) # {{{
+  def OnlineUser.touch(user, ip)
     raise TypeError unless user.is_a?(User) || user.nil?
     username = user.username unless user.nil?
     raise if [@@guest, @@record].include? username
     username = @@guest if user.nil?
     conds    = sanitize_sql(["username = '%s' AND ip = '%s'", username, ip])
     OnlineUser.delete_all(conds)
-    ou          = OnlineUser.new 
+    ou          = OnlineUser.new
     ou.username = username
     ou.ip       = ip
     ou.time     = Time.now.to_i
     ou.save
-  end # }}}
-  def OnlineUser.cleanup(time=5.minutes) # {{{
+  end
+  def OnlineUser.cleanup(time=5.minutes)
     conds = sanitize_sql(["username != '%s' AND time < %s", @@record,
       Time.now.to_i - time])
     OnlineUser.delete_all(conds)
-  end # }}}
-  def OnlineUser.online # {{{
-    usernames = []
-    users     = []
-    OnlineUser.find_all.each do |ou|
-      unless [@@guest, @@record].include? ou.username
-        usernames << ou.username 
-      end
-    end
-    usernames.sort.uniq.each do |un|
-      users << User.find_by_username(un)
-    end
-    users.compact
-  end # }}}
-  def OnlineUser.guests_count # {{{
+  end
+  def OnlineUser.guests_count
     conds = sanitize_sql(["username LIKE '%s'", @@guest])
     OnlineUser.count(conds)
-  end # }}}
+  end
+  def OnlineUser.online
+    users = []
+    OnlineUser.find(
+      :all, :joins => 'LEFT JOIN xmb_members
+        ON xmb_members.username = xmb_whosonline.username',
+      :order => 'xmb_members.username'
+    ).each do |ou|
+      unless [@@guest, @@record, nil].include? ou.username
+        u          = User.new
+        u.username = ou.username
+        u.id       = ou.uid
+        users << u
+      end
+    end
+    users
+  end
 end
