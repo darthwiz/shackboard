b21abdfcc4e69b7b27d06d67cac02b0ba7c0fefd
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2008-07-20 19:27:31 +0000

    R    trunk/app/models/forum.rb   - Backported the new Forum#latest_topics (gets the latest topics in all     sub-forums with a single query).
    M    trunk/app/views/layouts/application.html.erb
      - Removed FCKEditor for now.
    
    
    git-svn-id: file:///var/lib/svn/shackboard/shackboard2/trunk@274 6c9d4111-2920-0410-ac48-c0788669fd8e

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -8,27 +8,23 @@ class Forum < ActiveRecord::Base
   has_many   :topics, :foreign_key => 'fid', :dependent => :destroy
   has_many   :posts,  :foreign_key => 'fid', :dependent => :destroy
   @@tree = nil
-  def container # {{{
+
+  def container
     self.forum
-  end # }}}
-  def acl # {{{
-    acl = AclMapping.map(self)
-    return acl if acl
-    acl = Acl.new
-    acl.can_read(['User', :any]) if (allowed.empty? && self.private == '')
-    acl.can_read(['User', :any]) if self[:type] == 'group' # XXX caution here
-    (allowed + moderator_ids + User.supermods).each do |uid|
-      uid = uid.id if uid.is_a? User
-      acl.can_read(['User', uid])
-      acl.can_moderate(['User', uid])
+  end
+
+  def can_read?(user)
+    userid = user.id if user.is_a? User
+    userid = user[1] if user.is_a? Array
+    return true if (allowed.empty? && self.private == '')
+    return true if allowed.include?(userid)
+    return true if moderator_ids.include?(userid)
+    User.supermods.each do |u|
+      return true if u.id = userid
     end
-    acl.save
-    am = AclMapping.new
-    am.associate(self, acl)
-    am.save
-    acl
-  end # }}}
-  def can_post?(user) # {{{
+  end
+
+  def can_post?(user)
     # FIXME This is a very basic implementation, only meant to pass the basic
     # tests.
     if user.is_a? Array
@@ -41,35 +37,70 @@ class Forum < ActiveRecord::Base
     perms = self[:postperm].split('|')
     perms.each_with_index { |i, j| perms[j] = i.to_i }
     return true if (perms[0] == 1 && user.is_a?(User))
-    return true if (perms[0] == 2 && self.acl.can_moderate?(user))
+    return true if (perms[0] == 2 && self.can_moderate?(user))
     return false
-  end # }}}
-  def moderators # {{{
+  end
+
+  def can_moderate?(user)
+    return false unless user.is_a? User
+    return true if User.admin_ids.include? user.id
+    return true if User.supermod_ids.include? user.id
+    return true if self.moderator_ids.include? user.id
+    return false
+  end
+
+  def moderators
     mods = []
     self.moderator.split(/,\s*/).each do |n|
       u = User.find_by_username(n.strip)
       mods << u unless u.nil?
     end
     mods
-  end # }}}
-  def moderator_ids # {{{
+  end
+
+  def moderator_ids
     mod_ids = []
     self.moderators.each { |u| mod_ids << u.id.to_i }
     mod_ids
-  end # }}}
-  def allowed # {{{
+  end
+
+  def allowed
     users = []
     self.userlist.split(/,\s*/).each do |n|
       u = User.find_by_username(n.strip)
       users << u.id.to_i unless u.nil?
     end
     users
-  end # }}}
-  def children # {{{
-    Forum.find(:all, :conditions => ['fup = ?', self.id],
-                     :order      => 'displayorder')
-  end # }}}
-  def topics(range) # {{{
+  end
+
+  def add_child(forum)
+    @children = [] if @children.nil?
+    @children << forum
+    @children.sort! { |a, b| a.displayorder.to_i <=> b.displayorder.to_i }
+  end
+
+  def children_cached
+    @children = [] unless @children
+    @children
+  end
+
+  def children
+    unless @@tree
+      @@tree = {}
+      forums = Forum.find(:all)
+      forums.each do |f|
+        @@tree[f.id] = f
+      end
+      @@tree.each_pair do |id, f|
+        if f.fup.to_i > 0
+          @@tree[f.fup.to_i].add_child(f)
+        end
+      end
+    end
+    @@tree[self.id].children_cached
+  end
+
+  def topics(range)
     raise TypeError, 'argument must be a Range' unless range.is_a? Range
     topics  = []
     seq     = range.begin
@@ -80,14 +111,17 @@ class Forum < ActiveRecord::Base
                          :offset     => range.begin,
                          :limit      => range.entries.length
     topics
-  end # }}}
-  def topics_count_cached # {{{
+  end
+
+  def topics_count_cached
     self[:threads]
-  end # }}}
-  def posts_count_cached # {{{
+  end
+
+  def posts_count_cached
     self[:posts]
-  end # }}}
-  def lastpost(what=nil) # {{{
+  end
+
+  def lastpost(what=nil)
     (time, username) = self[:lastpost].split(/\|/, 2)
     time             = Time.at(time.to_i)
     case what
@@ -102,66 +136,44 @@ class Forum < ActiveRecord::Base
     else
       return nil
     end
-  end # }}}
-  def Forum.tree(root=nil) # {{{
-    return @@tree if (@@tree && root == nil)
-    unless root.is_a? Forum
-      root    = Forum.new
-      root.id = 0
-    end
-    tree = []
-    root.children.each do |f|
-      tree << [f, Forum.tree(f)]
-    end
-    if root.id == 0
-      tree = [[], tree]
-      @@tree = tree unless @@tree
-    end
-    tree
-  end # }}}
-  def Forum.set_tree(tree=nil) # {{{
-    @@tree = tree if tree.is_a? Array
-  end # }}}
-  def Forum.subtree(f, tree=Forum.tree) # {{{
-    if tree[0] == f
-      return [f, tree[1]]
-    else
-      tree[1].each do |t|
-        r = Forum.subtree(f, t)
-        return r if r[0] == f
-      end
-    end
-  end # }}}
-  def fix_counters # {{{
+  end
+
+  def fix_counters
     self[:threads] = Forum.find(self.id).topics_count
     self[:posts]   = Forum.find(self.id).posts_count
     self.save
     [ self[:threads], self[:posts] ]
-  end # }}}
-  def latest_topics(n=5, parms={}) # {{{
+  end
+
+  def latest_topics(n=5, parms={})
+    ids = [ self.id ]
+    if parms[:include_sub]
+      self.children.each do |f|
+        ids << f.id
+      end
+    end
+    id_list = "(" + ids.join(', ') + ")"
     topics = Topic.find(:all,
-               :conditions => ['fid = ? AND deleted IS NULL', self.id],
+               :conditions => "fid IN #{id_list} AND deleted IS NULL",
                :order      => 'lastpost DESC',
                :limit      => n
     )
-    if parms[:include_sub]
-      self.children.each do |f|
-  if f.acl.can_read?(parms[:user])
-    topics += Forum.latest_topics(f.id, n, :include_sub => true)
   end
-      end
-    end
-    topics.sort! { |a, b| a.lastpost <=> b.lastpost }
-    topics.reverse[0...n]
-  end # }}}
-  def Forum.latest_topics(id, *args) # {{{
+
+  def Forum.latest_topics(id, *args)
     f = Forum.find(id)
     f.latest_topics(*args)
-  end # }}}
-  def move_to_sub(forum) # {{{
+  end
+
+  def Forum.reset_cache!
+    @@tree = nil
+  end
+
+  def move_to_sub(forum)
     raise ArgumentError unless forum.is_a? Forum
     self.fup = forum.id
     self[:type] = "sub"
     self.save
-  end # }}}
+  end
+
 end
