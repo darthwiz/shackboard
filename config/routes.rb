773954a502f556328bb7a57f022644de869f0c38
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2008-08-20 00:23:29 +0000

    Merged changeset 303 from development branch.
    git-svn-id: file:///var/lib/svn/shackboard/shackboard2/trunk@304 6c9d4111-2920-0410-ac48-c0788669fd8e

--- config/routes.rb
+++ config/routes.rb
@@ -26,10 +26,11 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
+  blog_reqs = { :username => /.*/ }
   map.root :controller => "welcome"
-  map.blog_list 'blogs/:username', :controller => 'blog', :action => 'list'
-  map.blog_view 'blogs/:username/:blog_label', :controller => 'blog', :action => 'view'
-  map.blog_view 'blogs/:username/:blog_label/:category_label', :controller => 'blog', :action => 'view'
+  map.blog_list 'blogs/:username', :controller => 'blog', :action => 'list', :requirements => blog_reqs
+  map.blog_view 'blogs/:username/:blog_label', :controller => 'blog', :action => 'view', :requirements => blog_reqs
+  map.blog_view 'blogs/:username/:blog_label/:category_label', :controller => 'blog', :action => 'view', :requirements => blog_reqs
 
   # See how all your routes lay out with "rake routes"
 
