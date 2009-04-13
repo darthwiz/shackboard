402516e8efcfceafc1a77c076533a0b2ad8243ea
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2009-04-13 00:00:19 +0000

    - Removed "static" Rails dependency. - Moved constant definitions up in environment.rb in order to make them work in   production environment.
    git-svn-id: file:///var/lib/svn/shackboard/shackboard2/trunk@365 6c9d4111-2920-0410-ac48-c0788669fd8e

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,27 @@
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+GLOBAL_ADM_GROUP = "administrators"
+FILEDB_PREFIX    = 'materiali_'
+FILEDB_ADM_GROUP = "materiali_adm"
+FILEDB_ICONS     = [
+  'http://www.studentibicocca.it/portale/materiali/images/posticons/',
+  [
+    ['supporti',           'supporti.gif'],
+    ['appunti studenti',   'studenti.gif'],
+    ['materiali docenti',  'docenti.gif'],
+    ["domande d'esame",    'domande.gif'],
+    ['dritte esami',       'dritte.gif'],
+    ['esercizi',           'esercizi.gif'],
+    ['schemi e riassunti', 'schemi.gif'],
+    ['altri materiali',    'altri.gif'],
+  ]
+]
+ANNOUNCEDB_PREFIX = 'TW_'
+COOKIE_DOMAIN     = '.studentibicocca.it'
+POST_BLOCK_SIZE   = 25
+TOPIC_BLOCK_SIZE  = 25
+
 # Frozen non-Rails gems
 Dir.glob(File.join(RAILS_ROOT, 'vendor', '*', 'lib')) do |path|
   $LOAD_PATH << path
@@ -68,23 +89,3 @@ end
 #ActionController::Base.fragment_cache_store = :mem_cache_store, '192.168.1.3'
 $KCODE = 'UTF8'
 
-GLOBAL_ADM_GROUP = "administrators"
-FILEDB_PREFIX    = 'materiali_'
-FILEDB_ADM_GROUP = "materiali_adm"
-FILEDB_ICONS     = [
-  'http://www.studentibicocca.it/portale/materiali/images/posticons/',
-  [
-    ['supporti',           'supporti.gif'],
-    ['appunti studenti',   'studenti.gif'],
-    ['materiali docenti',  'docenti.gif'],
-    ["domande d'esame",    'domande.gif'],
-    ['dritte esami',       'dritte.gif'],
-    ['esercizi',           'esercizi.gif'],
-    ['schemi e riassunti', 'schemi.gif'],
-    ['altri materiali',    'altri.gif'],
-  ]
-]
-ANNOUNCEDB_PREFIX = 'TW_'
-COOKIE_DOMAIN     = '.studentibicocca.it'
-POST_BLOCK_SIZE   = 25
-TOPIC_BLOCK_SIZE  = 25
