0fe6c30167a3231240881f7729da7d158a0cc76e
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2009-04-19 05:23:50 +0000

    - Added meaningful prefix to resources' routes.
    git-svn-id: file:///var/lib/svn/shackboard/shackboard2/trunk@389 6c9d4111-2920-0410-ac48-c0788669fd8e

--- config/routes.rb
+++ config/routes.rb
@@ -25,9 +25,9 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   map.resources :users
-  map.resources :posts
-  map.resources :blog_posts, :has_many => :blog_comments
-  map.resources :blog_comments
+  map.resources :posts, :path_prefix => 'forum'
+  map.resources :blog_posts, :has_many => :blog_comments, :path_prefix => 'blog'
+  map.resources :blog_comment, :path_prefixs => 'blog'
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
