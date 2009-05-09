961d11479813bee2bbbcd91a0f97e0e18829827f
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2009-05-09 09:50:14 +0000

    - Improved the BB parser to better support URLs and not get spoilers wrong most   of the time like before.
    git-svn-id: file:///var/lib/svn/shackboard/shackboard2/trunk@477 6c9d4111-2920-0410-ac48-c0788669fd8e

--- lib/bb_text.rb
+++ lib/bb_text.rb
@@ -6,6 +6,7 @@ class BbText
   end
 
   def to_html(params={})
+    require 'md5'
     s            = Sanitizer.sanitize_bb(@text)
     spoiler_base = params[:spoiler_id].to_i
     spoiler_id   = 0
@@ -18,26 +19,29 @@ class BbText
       match.strip!
       " <img src=\"#{match}\" alt=\"(img)\"/> "
     end
-    s.gsub!(/\[i\]/i, "<i>")
-    s.gsub!(/\[\/i\]/i, "</i>")
-    s.gsub!(/\[b\]/i, "<b>")
-    s.gsub!(/\[\/b\]/i, "</b>")
-    s.gsub!(/\[u\]/i, "<u>")
-    s.gsub!(/\[\/u\]/i, "</u>")
-    s.gsub!(/\[code\]/i, "<div class=\"code\">")
-    s.gsub!(/\[\/code\]/i, "</div>")
-    s.gsub!(/\[quote\]/i, "<div class=\"quote\">")
-    s.gsub!(/\[\/quote\]/i, "</div>")
-    s.gsub!(/\[spoiler\]/i) do |match|
-      spoiler_id += 1
-      dom_id = "spoiler_#{spoiler_base}_#{spoiler_id}"
-      "<div class=\"spoiler\">
-        <a class=\"spoiler\" href=\"javascript:void(0);\" onclick=\"e = document.getElementById('#{dom_id}'); if (e.style.display == 'none') { e.style.display = ''} else { e.style.display = 'none' }\">spoiler</a>
-        <div style=\"display: none;\" id=\"#{dom_id}\">"; 
+    s.gsub!(/\[i\](.*?)\[\/i\]/im, "<i>\\1</i>")
+    s.gsub!(/\[b\](.*?)\[\/b\]/im, "<b>\\1</b>")
+    s.gsub!(/\[u\](.*?)\[\/u\]/im, "<u>\\1</u>")
+    while s =~ /\[code\](.*)\[\/code\]/im
+      s.gsub!(/\[code\](.*)\[\/code\]/im, "<div class=\"code\">\\1</div>")
     end
-    s.gsub!(/\[\/spoiler\]/i, "</div></div>")
+    while s =~ /\[quote\](.*)\[\/quote\]/im
+      s.gsub!(/\[quote\](.*)\[\/quote\]/im, "<div class=\"quote\">\\1</div>")
+    end
+    while s =~ /\[spoiler\](.*)\[\/spoiler\]/im
+      s.gsub!(/\[spoiler\](.*)\[\/spoiler\]/im) do |match|
+        spoiler_id += 1
+        dom_id = "spoiler_#{MD5.md5(match).to_s[0..4]}_#{spoiler_id}"
+        "<div class=\"spoiler\">
+          <a class=\"spoiler\" href=\"javascript:void(0);\" onclick=\"e = document.getElementById('#{dom_id}'); if (e.style.display == 'none') { e.style.display = ''} else { e.style.display = 'none' }\">spoiler</a>
+          <div style=\"display: none;\" id=\"#{dom_id}\">
+          #{$1}
+          </div>
+        </div>"; 
+      end
+    end
     s.gsub!(/(^|[>[:space:]\n])([[:alnum:]]+):\/\/([^[:space:]]*)([[:alnum:]#?\/&=])([<[:space:]\n]|$)/, "\\1<a href=\"\\2://\\3\\4\" target=\"_blank\">\\2://\\3\\4</a>\\5")
-    s.gsub!(/\[url=([^\]]*)\](.*)\[\/url\]/, "<a href=\"\\1\" title=\"\\1\">\\2</a>")
+    s.gsub!(/\[url=([^\]]*)\](.*?)\[\/url\]/, "<a href=\"\\1\" title=\"\\1\">\\2</a>")
     @smileys.each do |sm|
       s.gsub!(/#{Regexp.escape(sm.code)}/, " <img src=\"#{sm.url}\" alt=\"\" /> ") unless sm.code.empty?
     end
