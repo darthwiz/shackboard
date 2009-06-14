c19280c8c28068b8241b14dac9553b66e32b3d85
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2009-06-14 18:56:58 +0000

    Fixed a big bad bug.
    git-svn-id: file:///var/lib/svn/shackboard/shackboard2/trunk@532 6c9d4111-2920-0410-ac48-c0788669fd8e

--- app/middleware/anonymize_filter.rb
+++ app/middleware/anonymize_filter.rb
@@ -6,10 +6,16 @@ class AnonymizeFilter
 
   def call(env)
     status, headers, response = @app.call(env)
-    new_response = ""
-    regex = /originariamente inviato da (#{User.anonymized_usernames.join('|')})/i
-    response.each { |i| new_response << i.gsub(regex, "Originariamente inviato da ***") }
-    [status, headers, [new_response]]
+    usernames = User.anonymized_usernames
+    if usernames.empty?
+      new_response = response
+    else
+      new_response = ""
+      regex = /originariamente inviato da (#{User.anonymized_usernames.join('|')})/i
+      response.each { |i| new_response << i.gsub(regex, "Originariamente inviato da ***") }
+      new_response = [ new_response ]
+    end
+    [status, headers, new_response]
   end
 
 end
