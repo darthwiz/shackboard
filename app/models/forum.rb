cc46af612d0326e3a420ac68dbeabfde1206c4f3
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2009-10-11 09:06:28 +0000

    Fixed a bug with forum posting permissions.
    git-svn-id: file:///var/lib/svn/shackboard/shackboard2/trunk@574 6c9d4111-2920-0410-ac48-c0788669fd8e

--- app/models/forum.rb
+++ app/models/forum.rb
@@ -42,6 +42,7 @@ class Forum < ActiveRecord::Base
     perms.each_with_index { |i, j| perms[j] = i.to_i }
     return true if (perms[0] == 1 && user.is_a?(User))
     return true if (perms[0] == 2 && self.can_moderate?(user))
+    return true if (perms[0] == 3 && user.is_supermod?)
     return false
   end
 
