531a21fb65895968fc61abb50b88b86884d63f68
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2010-02-16 20:46:58 +0000

    Even more precise CMS page tagging.
    git-svn-id: file:///var/lib/svn/shackboard/shackboard2/trunk@640 6c9d4111-2920-0410-ac48-c0788669fd8e

--- app/models/cms_page.rb
+++ app/models/cms_page.rb
@@ -6,28 +6,50 @@ class CmsPage < ActiveRecord::Base
   validates_uniqueness_of :slug
   default_scope :conditions => { :deleted_by => nil }
 
-  named_scope :tagged_with, lambda { |tag|
-    raise TypeError unless tag.is_a?(String)
-    cpt = self.table_name
-    cn  = self.to_s
-    tt  = Tag.table_name
-    {
-      :joins      => "INNER JOIN #{tt} AS tw_t ON #{cpt}.id = tw_t.obj_id",
-      :conditions => [ 'tw_t.obj_class = ? AND tw_t.tag = ?', cn, tag ],
-    }
-  }
-
-  def tag_with(tags)
+  named_scope :tagged_with, lambda { |tags|
     tags = tags.split(/,/).collect(&:slugify) if tags.is_a?(String)
     raise TypeError unless tags.is_a?(Array)
+    tags = tags.uniq.sort
+    cpt  = self.table_name
+    cn   = self.to_s
+    tt   = Tag.table_name
+    if tags.empty?
+      tags_cond  = 'tw_t.tag IS NULL'
+      tags_count = 1
+    else
+      tags_cond  = "tw_t.obj_class = '#{cn}' AND tw_t.tag IN (" + tags.collect { |i| "'#{i}'" }.join(', ') + ')'
+      tags_count = tags.size
+    end
+    {
+      :select     => "#{cpt}.*, COUNT(*) AS tag_count",
+      :joins      => "LEFT JOIN #{tt} AS tw_t ON #{cpt}.id = tw_t.obj_id",
+      :conditions => tags_cond,
+      :group      => "#{cpt}.id",
+      :having     => "tag_count = #{tags_count}",
+    }
+    # OPTIMIZE: the left join is probably unsuitable for large datasets and an
+    # inner join would be better, along with a special handling of the empty
+    # tag set (which is handled correctly by the left join right now)
+  }
+
+  def tag_with(tags, options={})
+    absolute = !!options[:absolute]
+    tags     = tags.split(/,/).collect(&:slugify) if tags.is_a?(String)
+    raise TypeError unless tags.is_a?(Array)
     obj_class    = self.class.to_s
-    tag_objs     = Tag.find(:all, :conditions => [ 'obj_class = ? AND obj_id = ?', obj_class, self.id ])
-    present_tags = tag_objs.collect(&:tag)
+    tag_objects  = Tag.find(:all, :conditions => [ 'obj_class = ? AND obj_id = ?', obj_class, self.id ])
+    present_tags = tag_objects.collect(&:tag)
     tags.each do |t|
       if !present_tags.include?(t)
         Tag.new(:obj_class => obj_class, :obj_id => self.id, :tag => t).save!
       end
     end
+    if absolute
+      tag_objects.each do |t|
+        t.destroy if !tags.include?(t.tag)
+      end
+    end
+    nil
   end
 
   def can_edit?(user)
