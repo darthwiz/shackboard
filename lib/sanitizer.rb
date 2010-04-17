6a8c056a4132ed65dfba8011b85493b128f88894
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2010-04-17 23:20:20 +0200

    Added [youtube] BB tag.

--- lib/sanitizer.rb
+++ lib/sanitizer.rb
@@ -81,7 +81,7 @@ class Sanitizer
   end
 
   def self.sanitize_bb(text)
-    ok_tags   = 'u, i, b, img, quote, code, spoiler, list, color, size, url, small, big, color, cms, block, index, span, list'
+    ok_tags   = 'u, i, b, img, quote, code, spoiler, list, color, size, url, small, big, color, cms, block, index, span, list, youtube'
     solo_tags = [ '*' ]
 
     # Build hash of allowed tags with allowed attributes
