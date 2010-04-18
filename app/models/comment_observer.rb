59152dce088e20f8fcc3b21a416a5417bcd9aaeb
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2010-04-18 15:47:25 +0200

    First basic notification test passes.

--- app/models/comment_observer.rb
+++ app/models/comment_observer.rb
@@ -1,6 +1,13 @@
 class CommentObserver < ActiveRecord::Observer
 
-  def after_create
+  def after_create(comment)
+    ntf = Notification.new(
+      :actor      => comment.user,
+      :recipient  => comment.commentable.user,
+      :notifiable => comment,
+      :kind       => 'new'
+    )
+    ntf.save
   end
 
 end
--- test/unit/notification_test.rb
+++ test/unit/notification_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class NotificationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "new blog post notification" do
+    post    = blog_posts(:wiz_1)
+    ark     = users(:ark)
+    wiz     = users(:wiz)
+    comment = Comment.new(:user => ark, :text => 'cool', :commentable => post, :ip_address => '1.2.3.4')
+    comment.save
+    ntf = Notification.find(:last)
+    assert_equal(ark, ntf.actor)
+    assert_equal(wiz, ntf.recipient)
+    assert_equal('new', ntf.kind)
+    assert_equal(comment, ntf.notifiable)
   end
+
 end
