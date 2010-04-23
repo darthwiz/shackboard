b5fdae0ee8b34c1eb854d8157773859dc4d46ce3
Luciano Di Lucrezia <luciano.dilucrezia@gmail.com>
2010-04-23 09:36:02 +0200

    Changed tag cloud position in layout.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   def index 
     @page_title    = "StudentiBicocca.it - il portale degli studenti della Bicocca"
     @announcements = Announcement.find_latest(5)
-    @popular_tags  = Forum.popular_tags(20)
+    @popular_tags  = Forum.popular_tags(30)
   end 
 
 end
